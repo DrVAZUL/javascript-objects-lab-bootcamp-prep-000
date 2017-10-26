bb2b7d1a5c397d7a948a015376e5a473c20601e9
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-26 00:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {lettuce: "alot", eggs: "even more", salt: "a single grain"}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
- 
-  return Object.assign({}, obj, { [key]: value })
+  Object.assign({}, obj, {[key]: value})
+  return obj
+
 }
